7aa674b9528a59afa8215773c9f1ee722860fd08
BandsWithLegends <adamosimpson21@gmail.com>
2018-09-06 13:50:33 -0700

    tweaking heroky deploy again

---
+++ config.js
@@ -0,0 +1,7 @@
+let config = {}
+
+config.secretString = "Apple Pie High Five"
+
+config.mongoString = "mongodb://BandsWithLegends:GrapeJelly@ds113749.mlab.com:13749/wepoll"
+
+module.exports = config;
